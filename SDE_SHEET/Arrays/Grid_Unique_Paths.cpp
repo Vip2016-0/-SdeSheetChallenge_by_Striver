7ae8640a1c2f33d9b89c107256d7f6ca6f9867e6
Vipin Kumar <65479359+Vip2016-0@users.noreply.github.com>
2022-07-03 23:23:28 +0530

    Create Grid_Unique_Paths.cpp

---
+++ SDE_SHEET/Arrays/Grid_Unique_Paths.cpp
@@ -0,0 +1,16 @@
+
+
+// Time Complexity :- O(n*m)
+// Space Complexity :- O(n*m)
+
+int uniquePaths(int n, int m) {
+    
+    vector<vector<int>> dp(n, vector<int>(m, 1));
+    
+    for(int i = 1 ; i < n ; i++){
+        for(int j = 1; j < m ; j++)
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    }
+    
+    return dp[n-1][m-1];
+}
